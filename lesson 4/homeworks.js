05e5ec5c5b774c711be5e0db44ff6bea508d68a3
hripman <rips.manukyan@gmail.com>
2017-04-23 13:12:28 +0400

    add homework

---
+++ lesson 4/homeworks.js
@@ -0,0 +1,44 @@
+function getIndex(arr, num) {
+	if(arr.length >0 && typeof(num) === "number") {
+		for(i = 0; i < arr.length; i++) {
+			if(arr[i] === num) {
+				return i;
+			}
+			//arr[i] === num ?? return i : countinue;   // ????
+		}
+	}
+	return -1;
+}
+
+function getRandomInt(maxInt) {
+	if(typeof(maxInt) === "number") {
+		return parseInt(String(Math.random() * (maxInt - 0) + 0));
+	}
+	throw "Error";
+}
+
+function getRandomMatrix(n, m) {
+	if(typeof(n) === "number" && typeof(m) === "number") {
+		var matrix = [];
+		var randIn;
+		for(var i = 0; i < n; ++i) {
+			matrix[i] = [];
+			for(var j = 0; j < m; ++j) {
+				matrix[i].push(getRandomInt(100));
+			}
+		}
+		return matrix
+	}
+	throw "Error";
+}
+
+
+function matrixIndexOf(matrix, num) {
+	for(var i = 0; i < matrix.length; ++i) {
+		var index = getIndex(matrix[i], num);
+		if(index !== -1) {
+			return i + "--" + index
+		}
+	}
+	return -1;
+}
