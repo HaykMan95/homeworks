3ee502bd25169c0e62b6f6bb82e7d52364b77bf1
HaykMan95 <hayk.man95@gmail.com>
2017-04-27 12:50:48 +0400

    Update createEnum.js
    fix

--- lesson 5/createEnum.js
+++ lesson 5/createEnum.js
@@ -3,11 +3,21 @@ function createEnum(arg) {
 	var index = 0;
 	for(var i = 0; i < arg.length; ++i) {
 		if(arg[i].name && arg[i].value) {
-			newObj[arg[i].name] = arg[i].value
+			newObj[arg[i].name]= {
+				enumerable: true,
+				writable: false,
+				configurable: false,
+				value: arg[i].value
+			}
 			index = arg[i].value;
 			index ++;
 		} else if(typeof(arg[i]) === "string") {
-			newObj[arg[i]] = index;
+			newObj[arg[i]] = {
+				enumerable: true,
+				writable: false,
+				configurable: false,
+				value: index
+			}
 			index ++;
 		}
 	}
