0a8db5949e1e900c85b08961d6b42b5ff8e86b3a
hripman <rips.manukyan@gmail.com>
2017-05-03 22:39:53 +0400

    fix

--- hwk-01-HaykMan95/solutions/getName.js
+++ hwk-01-HaykMan95/solutions/getName.js
@@ -2,9 +2,6 @@
     'use strict';
 
     window.getName = function(name) {
-        if(name.slice(0, 8) === '[object ') {
-            return name.slice(8, -1);
-        }
-        throw 'Error!!';
+        return name.slice(8, -1);
     };
 }());
