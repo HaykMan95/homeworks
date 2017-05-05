726f90b594fdb06fbf2fa7deabf23c0883ac014f
hripman <rips.manukyan@gmail.com>
2017-05-06 00:18:55 +0400

    fix

--- lesson 6/range.js
+++ lesson 6/range.js
@@ -1,10 +1,36 @@
 (function(that){
     'use strict'
     that.namespace = that.namespace || {};
+
+    function cmp(op, range1, range2) {
+        switch (op) {
+            case '>':
+                return range1.to - range1.from > range2.to - range2.from;
+                break;
+            case '<':
+                return range1.to - range1.from < range2.to - range2.from;
+                break;
+            case '>=':
+                return range1.to - range1.from >= range2.to - range2.from;
+                break;
+            case '<=':
+                return range1.to - range1.from <= range2.to - range2.from;
+                break;
+            case '==':
+                return range1.to - range1.from == range2.to - range2.from;
+                break;
+        }
+    }
+
     that.namespace.Range = function(x,y) {
-        this.from = x;
-        this.to = y;
-        
+        if(toString.call(x).slice(8,-1) === 'Object') {
+            return new namespace.Range(x.from, x.to);
+        } else if(toString.call(x).slice(8,-1) === 'Array' && x.length === 2) {
+            return new namespace.Range(x[0], x[1]);
+        } else if(arguments.length === 2) {
+            this.from = x;
+            this.to = y;
+        }
     }
     that.namespace.Range.isInRange = function(range, x) {
         if(range.from <= x && x <= range.to) {
@@ -18,5 +44,32 @@
         }
         return false;
     }
+
+    that.namespace.Range.prototype.gt = function(range) {
+        return cmp(">", this, range);
+    }
+
+    that.namespace.Range.prototype.lt = function(range) {
+        return cmp("<", this, range);
+    }
+
+    that.namespace.Range.prototype.gte = function(range) {
+        return cmp(">=", this, range);
+    }
+
+    that.namespace.Range.prototype.lte = function(range) {
+        return cmp("<=", this, range);
+    }
+
+    that.namespace.Range.prototype.eq = function(range) {
+        return cmp("==", this, range);
+    }
+
+    that.namespace.Range.prototype.compare = function(range) {
+        // return cmp(">", this, range)? 1: cmp("<", this, range)? -1: 0 ;
+        return + cmp(">", this, range) - cmp("<", this, range) + 0;
+    }
+
+
 
 }(this))
